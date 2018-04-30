5ca2733fa20db9280eb6a6b03e6683a44aff1951
Greg <ghunkins@users.noreply.github.com>
2018-04-30 10:45:58 -0400

    neuraljob.sh more memory

--- Code/neuraljob.sh
+++ Code/neuraljob.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu
 #SBATCH -t 1-00:00:00
 #SBATCH --job-name=VIRAL
-#SBATCH --mem=30GB 
+#SBATCH --mem=128GB 
 #SBATCH --output=output_n_%j.txt
 #SBATCH -e error_n_%j.txt
 #SBATCH --gres=gpu:2
