90d1e0b7a7cb37835bb9fe103e87ca5f499a7078
Greg <ghunkins@users.noreply.github.com>
2018-05-01 15:21:20 -0400

    job update

--- Code/neuraljob.sh
+++ Code/neuraljob.sh
@@ -2,9 +2,8 @@
 #SBATCH -p gpu
 #SBATCH -t 1-00:00:00
 #SBATCH --job-name=VIRAL
-#SBATCH --mem=124GB 
 #SBATCH --output=output_n_%j.txt
 #SBATCH -e error_n_%j.txt
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 source activate VIRAL
 python neuralmain.py
