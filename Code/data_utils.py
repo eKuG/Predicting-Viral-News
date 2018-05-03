9025fa4cecc0429a139573872b56e0aafb9a74fb
Greg <ghunkins@users.noreply.github.com>
2018-05-03 17:57:12 -0400

    update for dynamic

--- Code/data_utils.py
+++ Code/data_utils.py
@@ -6,6 +6,8 @@ Class: Data Mining | CSC 440
 Programmer: Gregory D. Hunkins 
 """
 import pandas
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
